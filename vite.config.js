210490b6606aa15e62654c71a67c62ead588a9f6
sadreammm <sadeem.meh10@gmail.com>
2024-09-22 23:14:57 +0400

    updated env

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/"
+  base: "/portfolio/",
+  define: {
+    'process.env.VITE_EMAILJS_SERVICE_ID': JSON.stringify(process.env.VITE_EMAILJS_SERVICE_ID)
+  }
 })
